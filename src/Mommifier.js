727d9a5fab13ad856a97ef709d2faa15232dc1c9
Yan Yu <yyan@thoughtworks.com>
2021-08-09 14:39:16 +0800

    YY: init exercise

---
+++ src/Mommifier.js
@@ -0,0 +1,5 @@
+class Mommifier {
+
+}
+
+export default Mommifier;
---
+++ src/test/Mommifier.test.js
@@ -0,0 +1,9 @@
+import Mommifier from "../Mommifier";
+
+describe('Mommifier Test', () => {
+    test('demo test', () => {
+        const mommifier = new Mommifier();
+
+        expect(1).toBe(1);
+    })
+})
