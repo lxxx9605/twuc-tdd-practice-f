5d3430decd0af9c3deb49c1672e0f57d53b49773
xiao <535408893@qq.com>
2021-08-10 11:09:54 +0800

    shoule return itself when input a consonant string

--- src/test/Mommifier.test.js
+++ src/test/Mommifier.test.js
@@ -3,6 +3,10 @@ import Mommifier from "../Mommifier";
 describe('Mommifier Test', () => {
     test('should get empty string when input enpty string', () => {
         const mommyFilter = new Mommifier('');
-        expect(mommyFilter.getResult())
+        expect(mommyFilter.getResult()).toBe('');
     })
+    test('should return itself when input a consonant string', () => {
+        const mommyFilter = new Mommifier('b');
+        expect(mommyFilter.getResult()).toBe('b');
+    });
 })
